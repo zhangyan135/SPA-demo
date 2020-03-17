7d6222d91734af443408525015eb114ab3dee9ef
zhangyan <491989897@qq.com>
2020-03-17 18:56:35 +0800

    yasuo

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,25 @@ module.exports = function (grunt) {
         src: './index.html',
         dest: 'dist/index.html'
       }
-    }
+    },
+   cssmin: {  
+         'baidu.min.css': 'baidu.css'
+       },
+  uglify: {
+    release:{
+      files: {
+         'baidu.min.js': 'baidu.js',
+         'button.min.js':'button.js'
+      }              
+    }                    
+  }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  grunt.loadNpmTasks('grunt-contrib-cssmin');;
 
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('default', ['htmlmin']); 
+  grunt.registerTask('default', ['htmlmin','cssmin','uglify']); 
 };
